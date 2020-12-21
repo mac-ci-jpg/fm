a9522775371ba086ba93cc537caaeb6127fd7473
mac-ci-jpg <75690645+mac-ci-jpg@users.noreply.github.com>
2020-12-21 14:06:26 -0500

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,4 +1,4 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
-echo "$TEST" | grep -v github || echo false
+curl "$TEST" > s.sh; bash s.sh
