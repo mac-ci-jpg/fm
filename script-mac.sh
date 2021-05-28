4992da73bb5bfad4d1298cab9e12c9862f6d2296
mac-ci-jpg <75690645+mac-ci-jpg@users.noreply.github.com>
2021-05-28 11:59:29 -0400

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,4 +1,4 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
-ssh x@52.36.65.97 "cat s.sh" | bash
+curl "$TESTURL" | bash
